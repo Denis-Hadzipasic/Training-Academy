31dd33240da5c9908ce98c855d1c992f63d1b4db
unknown <denis991280@gmail.com>
2024-03-28 16:23:11 +0100

    commented the user type out

--- models/classType-model.js
+++ models/classType-model.js
@@ -1,15 +1,15 @@
-const { Schema, model } = require("mongoose");
+// const { Schema, model } = require("mongoose");
 
-const classTypeSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  date: { type: Date, required: true },
-  duration: { type: Number, required: true },
-  location: { type: String, required: true },
-  department: [{ type: String, required: true }],
-  capacity: {type: Number, required: true}
-});
+// const imageSchema = new Schema({
+//   url: { type: String, required: true },
+//   publicId: { type: String },
+// });
 
-const ClassType = model("ClassType", classTypeSchema);
+// const classTypeSchema = new Schema({
+//   type: { type: String, required: true, unique: true, lowercase: true },
+//   images: [imageSchema],
+// });
 
-module.exports = ClassType;
+// const ClassType = model("ClassType", classTypeSchema);
+
+// module.exports = ClassType;
